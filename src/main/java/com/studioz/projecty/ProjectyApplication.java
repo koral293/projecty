bb394f00262a8439ffef80535a2a4f67b7b7edad
Damian <damkar2222@gmail.com>
2022-05-01 23:10:55 +0200

    Halo 2

--- src/main/java/com/studioz/projecty/ProjectyApplication.java
+++ src/main/java/com/studioz/projecty/ProjectyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectyApplication {
 //halo
 	// halo
+	// halo halo
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectyApplication.class, args);
 	}
