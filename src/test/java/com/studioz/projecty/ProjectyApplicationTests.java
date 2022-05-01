fc03ebef9c2a99b5a52d811057f21afe82c98a27
Tomasz Cabaj <tomekcabaj@gmail.com>
2022-05-01 22:54:13 +0200

    Initial commit

---
+++ src/test/java/com/studioz/projecty/ProjectyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studioz.projecty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
